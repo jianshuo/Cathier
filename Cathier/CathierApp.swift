c8c543c0450dc148be7cba9ea8b85346105726f1
Jianshuo Wang <jianshuo@hotmail.com>
2026-03-13 10:38:15 +0800

    Initial Commit

---
+++ Cathier/CathierApp.swift
@@ -0,0 +1,17 @@
+//
+//  CathierApp.swift
+//  Cathier
+//
+//  Created by Jianshuo Wang on 2026/3/13.
+//
+
+import SwiftUI
+
+@main
+struct CathierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
